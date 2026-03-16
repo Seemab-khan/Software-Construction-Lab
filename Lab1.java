7fe0aebac57c422edc4e4f09c2ae51cfc4ce9fa4
seemab khan <seemab@gmail.com>
2026-03-16 07:43:12 +0500

    java updated SCD

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,5 @@
 public class Lab1{
     public static void main(String[] args){
-        System.out.println("JAVA is not bakwas.");
+        System.out.println("This is our First Java Lab of SCD");
     }
 }
