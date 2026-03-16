476e81bee307d0ecf68d026acf48cb43e2f8a12d
seemab khan <seemab@gmail.com>
2026-03-16 09:37:49 +0500

    " commiting the updated java code"

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args){
+Students s1 = new Students();
+
+s1.name = "Ali";
+
+s1.rollNumber = 101;
+
+Students s2 = new Students();
+
+s2.name = "Ahmed";
+
+s2.rollNumber = 102;
+
+s1.displayInfo();
+s2.displayInfo();
+
+    }
+    
+}
